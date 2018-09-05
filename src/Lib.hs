44fa4b11ad719b00895ecf17f5ad37f1a95fba5a
Matthieu Jacquot <matthieu.jacquot@schibsted.com>
2018-09-05 18:27:00 +0200

    improve uc style & add pseudo implem

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,7 @@ module Lib
   ( someFunc
   ) where
 
+import Usecases.UC
+
 someFunc :: IO ()
-someFunc = print "sadlkfj"
+someFunc = test
--- src/Usecases/UC.hs
+++ src/Usecases/UC.hs
@@ -6,11 +6,12 @@ module Usecases.UC
   , UserReadWriter(..)
   , ArticleReadWriter(..)
   , Slugger(..)
+  , test
   ) where
 
+import Control.Monad.Identity
 import Control.Monad.Trans.Either
 import Control.Monad.Trans.Except
---  , PostArticleResp(..)
 import Data.Maybe
 import Domain.Article
 import Domain.User
@@ -18,14 +19,14 @@ import Domain.User
 class Monad m =>
       UserReadWriter m
   where
-  userCreate :: String -> String -> String -> m (Either String (Maybe Domain.User.User))
+  userCreate :: String -> String -> String -> m (Either String Domain.User.User)
   userGetByName :: String -> m (Either String (Maybe Domain.User.User))
 
 class Monad m =>
       ArticleReadWriter m
   where
-  articleCreate :: Domain.Article.Article -> m (Either String (Maybe Domain.Article.Article))
-  articleSave :: Domain.Article.Article -> m (Either String (Maybe Domain.Article.Article))
+  articleCreate :: Domain.Article.Article -> m (Either String Domain.Article.Article)
+  articleSave :: Domain.Article.Article -> m (Either String Domain.Article.Article)
   articleGetBySlug :: String -> m (Either String (Maybe Domain.Article.Article))
 
 class Monad m =>
@@ -40,7 +41,8 @@ class Monad m =>
 
 instance (UserReadWriter m, ArticleReadWriter m, Slugger m, Monad m) => BusinessLogic m where
   postArticle username article = do
-      res <- runExceptT $ do
+    res <-
+      runExceptT $ do
         _user <- ExceptT $ userGetByName username
         case _user of
           Nothing -> return (Left "user not found")
@@ -50,10 +52,28 @@ instance (UserReadWriter m, ArticleReadWriter m, Slugger m, Monad m) => Business
             case articleInStorage of
               Just _ -> return (Left "article already exists with the same slug")
               Nothing -> do
-                _newArticle <- EitherT $ articleCreate article {slug = slug}
-                case _newArticle of
-                  Nothing -> return (Left "no article returned on creation")
-                  Just newArticle -> return (Right (user, newArticle))
-      case res of
-       Left err -> return (Left err)
-       Right u -> return u
+                newArticle <- EitherT $ articleCreate article {slug = slug}
+                return (Right (user, newArticle))
+    case res of
+      Left err -> return (Left err)
+      Right u -> return u
+
+
+
+
+instance Monad m => UserReadWriter (IdentityT m) where
+  userCreate _ _ _ = return (Left "oops")
+  userGetByName _ = return (Left "oops")
+
+instance Monad m => ArticleReadWriter (IdentityT m) where
+  articleCreate _ = return (Left "oops")
+  articleSave _ = return (Left "oops")
+  articleGetBySlug _ = return (Left "oops")
+
+instance Monad m => Slugger (IdentityT m) where
+  newSlug _ = return (Left "oops")
+
+test :: IO ()
+test = do
+  res <- runIdentityT (postArticle "username" Article {})
+  print res
